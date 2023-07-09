1ddba54ffedebf85fae6e7b37ba1f0dc8dff76cb
azad <md1azad0@gmail.com>
2023-07-09 14:55:11 +0600

    de

---
+++ assets/js/data.js
@@ -0,0 +1,148 @@
+const post = [
+  {
+    id: 1,
+    images: `https://staticg.sportskeeda.com/editor/2023/03/73a71-16788913758937-1920.jpg?w=668`,
+
+    content: `আলহামদুলিল্লাহ অসাধারণ একটা দিন কাটলো  2 Win and 2 Match Man of The Match ঢাকা  কেরানীগঞ্জ 🥰🥰`,
+    timeago: `29m`,
+    like: 220,
+    comment: 96,
+    author: {
+      name: `Md Azad Khan`,
+      photo: `https://staticg.sportskeeda.com/editor/2023/03/73a71-16788913758937-1920.jpg?w=668`,
+    },
+  },
+
+  {
+    id: 2,
+    images: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/60/b1/42/medina.jpg?w=700&h=500&s=1`,
+
+    content: `মদিনা💓`,
+    timeago: `19m`,
+    like: 520,
+    comment: 596,
+    author: {
+      name: `Mohammed Jahid`,
+      photo: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/60/b1/42/medina.jpg?w=700&h=500&s=1`,
+    },
+  },
+
+  {
+    id: 3,
+    images: `https://static.toiimg.com/thumb/msid-99583904,width-1070,height-580,imgsize-43976,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg`,
+
+    content: `ঈদ মোবারক 💜`,
+    timeago: `11m`,
+    like: 20,
+    comment: 56,
+    author: {
+      name: `MD Rasel Sardar`,
+      photo: `https://static.toiimg.com/thumb/msid-99583904,width-1070,height-580,imgsize-43976,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg`,
+    },
+  },
+
+  {
+    id: 4,
+    images: `https://www.brainyquote.com/photos_tr/en/g/ghostfacekillah/974241/ghostfacekillah1.jpg`,
+
+    content: `No Caption... Just Chill...`,
+    timeago: `just now`,
+    like: 30,
+    comment: 56,
+    author: {
+      name: `Ariful Islam Noyon`,
+      photo: `https://www.brainyquote.com/photos_tr/en/g/ghostfacekillah/974241/ghostfacekillah1.jpg`,
+    },
+  },
+
+  {
+    id: 5,
+    images: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/332305391_1637363363400638_8306703697728198959_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8631f5&_nc_eui2=AeFXRIMin8TomXJe66vFDQo69VfTA5oolqT1V9MDmiiWpPd3u3f4-bQ0baINbr-pBtSwJ_00rHeAbd1DIzuHhYKr&_nc_ohc=4o84V2Bkg0gAX-rbNdt&_nc_ht=scontent.fdac144-1.fna&oh=00_AfC4oFsiI5KXtvweVDCyy8WmYeEzsfZIUMG8bdePpcKB0g&oe=64AFDB34`,
+
+    content: `মৃত্যুর পর একজন মুসলিমের জন্য, বরকতময় এবং পুণ্যের কাজ হলো, পৃথিবীতে দ্বীনদার সন্তান রেখে যাওয়া।
+    ( সহীহ বুখারী ২৫৫৮ )
+    হে আল্লাহ তুমি এই সন্তান কে বাবার মতো দ্বীনের পথে কবুল করে নিন। আমিন।`,
+    timeago: `56m`,
+    like: 90,
+    comment: 59,
+    author: {
+      name: `World In Bangladesh`,
+      photo: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/357051082_637943048368836_2141523010597790873_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8631f5&_nc_eui2=AeHA22Zpw9Z4zz4ZZjFl0ZzwlumpMySPvwmW6akzJI-_CcPF4x88OtLypEYPUH_1lJSgmMeDTHnnjJC_zVwcCc2E&_nc_ohc=4YZfCeDt0hkAX-ZmoNQ&_nc_ht=scontent.fdac144-1.fna&oh=00_AfBAwEMh0_RQL-4KTDVUhN9Yd9Nal9QOvyYMOQcRX89e1A&oe=64A1ECE8`,
+    },
+  },
+
+  {
+    id: 6,
+    image: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/356705553_1392155421567251_5254785080282242934_n.jpg?stp=cp6_dst-jpg_p526x296&_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeF2QARmXxuWQOTec9eGnLM8sRkCBVx5GxuxGQIFXHkbG_pI402_Fc2Aw1bZp6M8ZyMcrpZ9waDLUTJfERs9CSNA&_nc_ohc=S21y0CZqDcEAX_TJ4o_&_nc_ht=scontent.fdac144-1.fna&oh=00_AfBpKAxlebUYKQrSBxZr9T6Yu4yFafUnmWumn1CWtIym3g&oe=64A3AC72`,
+
+    content: `মিষ্টি পান 🥘 খাইতে গেছিলাম ভাই সেই স্বাদ।`,
+    timeago: `13m`,
+    like: 52,
+    comment: 796,
+    author: {
+      name: `Adnan Habib Nayeem`,
+      photo: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/356705553_1392155421567251_5254785080282242934_n.jpg?stp=cp6_dst-jpg_p526x296&_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeF2QARmXxuWQOTec9eGnLM8sRkCBVx5GxuxGQIFXHkbG_pI402_Fc2Aw1bZp6M8ZyMcrpZ9waDLUTJfERs9CSNA&_nc_ohc=S21y0CZqDcEAX_TJ4o_&_nc_ht=scontent.fdac144-1.fna&oh=00_AfBpKAxlebUYKQrSBxZr9T6Yu4yFafUnmWumn1CWtIym3g&oe=64A3AC72`,
+    },
+  },
+
+  {
+    id: 7,
+    images: `https://i.ytimg.com/vi/TvcYiRuGhpY/maxresdefault.jpg`,
+
+    content: `আলহামদুলিল্লাহ ভালো একটা দিন কাটলো 
+    সকালে ফুটবল বিকালে ক্রিকেট 🥰😍🤩`,
+    timeago: `19m`,
+    like: 520,
+    comment: 96,
+    author: {
+      name: `Md Azad Khan`,
+      photo: `https://i.ytimg.com/vi/TvcYiRuGhpY/maxresdefault.jpg`,
+    },
+  },
+
+  {
+    id: 8,
+    images: `https://content.invisioncic.com/u312729/monthly_2020_08/fiverr.JPG.ed2313390e0a0df1436d72efe8f86a9a.JPG`,
+
+    content: `আলহামদুলিল্লাহ, ১০০ তম রিভিউ ❤ `,
+    timeago: `9m`,
+    like: 53,
+    comment: 59,
+    author: {
+      name: `Saifuzzaman Saimun`,
+      photo: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/277303720_4760779600686385_6949269697193218044_n.jpg?stp=dst-jpg_s720x720&_nc_cat=107&cb=99be929b-3346023f&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGUit3INMk3uq-5Tm82vvvtOczlHd6_JYs5zOUd3r8li8O6QvrnIUm_w9gC52ITNeybD6ihRus-uG_PthbXYl0p&_nc_ohc=vStHtEQwQF4AX8AyuZz&_nc_ht=scontent.fdac144-1.fna&oh=00_AfAsLDSO_RZ2HdBB3E4c5Wenpj1xSHjrvFRzIa9zLte1MQ&oe=64A2418F`,
+    },
+  },
+
+  {
+    id: 9,
+    images: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/356244008_872507190834191_3035547315150244219_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGMBAvXqpLGhNr05lOfFQ7PFGg7dmozD7AUaDt2ajMPsE-lpNy4VPr6oRUOp4Cf6OwU_VBAaK5EIbsPDn1fHZmU&_nc_ohc=98k4Q1mUnD0AX8tuyha&_nc_ht=scontent.fdac144-1.fna&oh=00_AfCnlqRlOWcA29Fy6zbjnSBUIBOFVDqL2nkkgAyQKWeEYg&oe=64A23550`,
+
+    content: `ইনশাআল্লাহ এসব মানুষের মুখে একদিন হাসি ফুটাবো
+    আল্লাহ তুমি আমাদের সেই তৌফিক দান করো আমিন`,
+    timeago: `1m`,
+    like: 10,
+    comment: 16,
+    author: {
+      name: `Md Shafiqul Islam Shameem
+      `,
+      photo: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/279860355_616055173146062_1853214100397106240_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEv3NoTJ0nWnLS-8v_r3Ctwre8yAcdUT42t7zIBx1RPjVx6-ebg8v-OW7LDFoSVtcoEBLK9ncIg2nQCIeA5xoj9&_nc_ohc=PfA85TsLDQ8AX_Bt2Xl&_nc_ht=scontent.fdac144-1.fna&oh=00_AfBa-bNm1l1XeE2nYy4X2dXxEHlvJPWWKmPhdI65wBYpoA&oe=64A2F7C1`,
+    },
+  },
+
+  {
+    id: 10,
+    image: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/356406593_1922585991454516_3371690379470370509_n.jpg?_nc_cat=101&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFki8xdt02iCEoNPfNPDAB46adE477kKErpp0TjvuQoSg0iI8LQPS4_nelJnjnRABWzyEEyCoVLju0VCuK8Psoy&_nc_ohc=AA1B3k5cQwcAX9mvY9n&_nc_ht=scontent.fdac144-1.fna&oh=00_AfD_RMEGfjsJOQ9H9OUsSANMQrEKFniKRigSC0MgQnYBsw&oe=64A2DC92`,
+
+    content: `পবিত্র হজ্জ পালনের আনুষ্ঠানিকতার শুরুতে মিনার তাবুতে প্রবেশ করেই স্ত্রীর সামনে মৃত্যুর কোলে ঢলে পড়লেন চট্টগ্রামের বোয়ালখালীর শফি সাহেব ইন্না-লিল্লাহ...রাজেউন। স্ত্রীকে মিনার তাবুতে রেখে শফি সাহেব এখন লাশ ঘরে, এই ইহরাম পরিধান অবস্থায় শফি সাহেবকে দাফন করা হবে। 
+    নির্বাক স্ত্রী এই হচ্ছে মৃত্যু যার যেখানে লেখা আছে সেখানে গিয়েই রবের ডাকে সাড়া দিতে হবে...আল্লাহ তার মেহমান হাজী শফি সাহেবকে জান্নাতুল ফেরদৌস দান করুন। আমিন।`,
+    timeago: `5m`,
+    like: 59,
+    comment: 586,
+    author: {
+      name: `Rovel Meah
+      `,
+      photo: `https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/310803872_1734284030284714_2796848396998715128_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFVLhsx-lwnrWg3rRRZRMS8K4CNlkbGxOYrgI2WRsbE5toPEZVzUTeIuxU8d9WD_TIYukZqGc8VB2mfRM1q5V6c&_nc_ohc=vQ1xiH6edjYAX-EuapP&_nc_ht=scontent.fdac144-1.fna&oh=00_AfBzSw8hwU-sJLlXEmkf5WgMwAXcDOijmyYM5X6Qy5e7hg&oe=64A25220`,
+    },
+  },
+];
